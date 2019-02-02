b82bfa4154d80a2fd6a040560a7db7dad951cc99
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-02-02 14:43:01 +0530

    Implemented camera capture on button click

--- app/src/main/java/technolifestyle/com/whereismytext/MainActivity.kt
+++ app/src/main/java/technolifestyle/com/whereismytext/MainActivity.kt
@@ -1,12 +1,35 @@
 package technolifestyle.com.whereismytext
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CAMERA_CAPTURE_CODE: Int = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        capture.setOnClickListener {
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            startActivityForResult(intent, CAMERA_CAPTURE_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_CAPTURE_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Timber.d("Data: %s", data)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
