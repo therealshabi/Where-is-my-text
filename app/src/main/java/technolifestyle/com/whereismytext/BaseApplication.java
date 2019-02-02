3ae4a3cd8761984e9c80ce57b271ab6a21e6fe37
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-02-02 14:06:06 +0530

    Create BaseApplication class and refactored Manifest

---
+++ app/src/main/java/technolifestyle/com/whereismytext/BaseApplication.java
@@ -0,0 +1,11 @@
+package technolifestyle.com.whereismytext;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
