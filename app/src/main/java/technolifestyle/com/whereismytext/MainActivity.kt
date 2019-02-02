b72b7c28a4cd6f09f81184b72ddf90b5c51f4626
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-02-02 18:39:13 +0530

    Extracted text from image using Firebase on device ML kit

--- app/src/main/java/technolifestyle/com/whereismytext/MainActivity.kt
+++ app/src/main/java/technolifestyle/com/whereismytext/MainActivity.kt
@@ -1,13 +1,17 @@
 package technolifestyle.com.whereismytext
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.Mode
 import com.otaliastudios.cameraview.PictureResult
 import kotlinx.android.synthetic.main.activity_main.*
-
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +28,9 @@ class MainActivity : AppCompatActivity() {
                 super.onPictureTaken(result)
                 result.toBitmap(900, 1800) { bitmap ->
                     imageView.setImageBitmap(bitmap)
+                    if (bitmap != null) {
+                        processBitmap(bitmap)
+                    }
                 }
             }
         })
@@ -41,6 +48,29 @@ class MainActivity : AppCompatActivity() {
             recaptureButton.isEnabled = false
             cameraView.visibility = View.VISIBLE
             imageView.visibility = View.GONE
+        }
+    }
+
+    private fun processBitmap(bitmap: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        val detector = FirebaseVision.getInstance()
+            .onDeviceTextRecognizer
+
+        detector.processImage(image)
+            .addOnSuccessListener { firebaseVisionText ->
+                Timber.d("Text: %s", firebaseVisionText.text)
+                processText(firebaseVisionText)
+            }
+            .addOnFailureListener {
+                Timber.e(it)
+            }
+    }
+
+    private fun processText(firebaseVisionText: FirebaseVisionText) {
+        for (block in firebaseVisionText.textBlocks) {
+            for (line in block.lines) {
+                Timber.d("Line: %s", line.text)
+            }
         }
     }
 }
